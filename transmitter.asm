a9da45feed54aa554ed033f06a6418a67685e4cd
Alexander Molotkov <43587588+Alexander-Molotkov@users.noreply.github.com>
2020-03-03 17:34:57 -0800

    Added basic transmitter functionality
    Input is polled from buttons 4-7 which transmit the bot code and the Right, Forward, Backward, and Left commands respectively.

--- transmitter.asm
+++ transmitter.asm
@@ -25,18 +25,20 @@
 .equ EngEnL = 7    ; Left Engine Enable Bit
 .equ EngDirR = 5    ; Right Engine Direction Bit
 .equ EngDirL = 6    ; Left Engine Direction Bit
-.equ Button0 = 0    ; Right Whisker Input Bit
-.equ Button1 = 1    ; Left Whisker Input Bit
+.equ Button0 = 4    
+.equ Button1 = 5    
+.equ Button2 = 6    
+.equ Button3 = 7    
 .equ WTime = 100    ; Time to wait in wait loop
 .equ robotID = $1A   ; Robot ID for this robot
 ; Use these action codes between the remote and robot
 ; MSB = 1 thus:
 ; control signals are shifted right by one and ORed with 0b10000000 = $80
 .equ MovFwd =  ($80|1<<(EngDirR-1)|1<<(EngDirL-1)) ;0b10110000 Move Forward Action Code
-.equ MovBck =  ($80|$00)        ;0b10000000 Move Backward Action Code
-.equ TurnR =   ($80|1<<(EngDirL-1))     ;0b10100000 Turn Right Action Code
-.equ TurnL =   ($80|1<<(EngDirR-1))     ;0b10010000 Turn Left Action Code
-.equ Halt =    ($80|1<<(EngEnR-1)|1<<(EngEnL-1))  ;0b11001000 Halt Action Code
+.equ MovBck =  ($80|$00)						   ;0b10000000 Move Backward Action Code
+.equ TurnR =   ($80|1<<(EngDirL-1))				   ;0b10100000 Turn Right Action Code
+.equ TurnL =   ($80|1<<(EngDirR-1))                ;0b10010000 Turn Left Action Code
+.equ Halt =    ($80|1<<(EngEnR-1)|1<<(EngEnL-1))   ;0b11001000 Halt Action Code
 
 ;***********************************************************
 ;* Start of Code Segment
@@ -59,14 +61,16 @@ INIT:
   out  SPH, mpr  ; Load SPH with high byte of RAMEND
  ;I/O Ports
   ;Initialize Port B for output
-  ldi mpr, (1<<EngEnL)|(1<<EngEnR)|(1<<EngDirR)|(1<<EngDirL)
+  ldi mpr, 0b11111111
   out DDRB, mpr ; Set the DDR register for Port B
   ldi mpr, $00
+  out PORTB, mpr
   ; Initialize Port D for input
-  ldi mpr, (0<<Button0)|(0<<Button1)
+  ldi mpr, (1<<PD3) | (0<<Button0) | (0<<Button1) | (0<<Button2) | (0<<Button3)
   out DDRD, mpr ; Set the DDR register for Port D
-  ldi mpr, (1<<Button0)|(1<<Button1)
-  out PORTD, mpr ; Set the Port D to Input with Hi-Z
+  ldi mpr, (1<<Button0)|(1<<Button1)|(1<<Button2)|(1<<Button3)
+  out PORTD, mpr
+
 
  ;USART1
   ;Initalize USART1
@@ -91,24 +95,121 @@ INIT:
 ;* Main Program
 ;***********************************************************
 MAIN:
-  ldi mpr, $00
-  out PORTB, mpr
-  ldi waitcnt, Wtime
-  rcall Wait
-USART_Transmit:
-  lds mpr, UCSR1A ; Loop until UDR1 is empty
-  sbrs mpr, UDRE1
-  rjmp USART_Transmit
-  ldi  data, $FF
-  sts UDR1, data ; Move data to transmit data buffer
-  ldi mpr, $FF
-  out PORTB, mpr
-  ldi waitcnt, Wtime
-  rcall Wait
+  in mpr, PIND
+
+  cpi mpr, 0b11011111
+  brne NEXT
+  rcall moveForward
+NEXT:
+
+  cpi mpr, 0b10111111
+  brne NEXT2
+  rcall moveBackward
+NEXT2:
+
+  cpi mpr, 0b01111111
+  BRNE NEXT3
+  rcall moveLeft
+NEXT3:
+  
+  cpi mpr, 0b11101111
+  BRNE NEXT4
+  rcall moveRight
+NEXT4:
+
   rjmp MAIN
+  
 ;***********************************************************
 ;* Functions and Subroutines
 ;***********************************************************
+
+moveForward:
+  ldi mpr, 0b00100000
+  out PORTB, mpr
+
+//Transmit robotid
+USART_Transmit_fwd1:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_fwd1
+  ldi  data, robotID
+  sts UDR1, data ; Move data to transmit data buffer
+
+//Transmit move code
+USART_Transmit_fwd2:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_fwd2
+  ldi  data, MovFwd
+  sts UDR1, data ; Move data to transmit data buffer
+ret
+
+
+
+moveBackward:
+  ldi mpr, 0b01000000
+  out PORTB, mpr
+
+//Transmit robotid
+USART_Transmit_bwd1:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_bwd1
+  ldi  data, robotID
+  sts UDR1, data ; Move data to transmit data buffer
+
+//Transmit move code
+USART_Transmit_bwd2:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_bwd2
+  ldi  data, MovBck
+  sts UDR1, data ; Move data to transmit data buffer
+ret
+
+moveLeft:
+  ldi mpr, 0b10000000
+  out PORTB, mpr
+
+//Transmit robotid
+USART_Transmit_lft1:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_lft1
+  ldi  data, robotID
+  sts UDR1, data ; Move data to transmit data buffer
+
+//Transmit move code
+USART_Transmit_lft2:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_lft2
+  ldi  data, TurnL
+  sts UDR1, data ; Move data to transmit data buffer
+ret
+
+
+moveRight:
+  ldi mpr, 0b00010000
+  out PORTB, mpr
+
+//Transmit robotid
+USART_Transmit_rht1:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_rht1
+  ldi  data, robotID
+  sts UDR1, data ; Move data to transmit data buffer
+
+//Transmit move code
+USART_Transmit_rht2:
+  lds mpr, UCSR1A ; Loop until UDR1 is empty
+  sbrs mpr, UDRE1
+  rjmp USART_Transmit_rht2
+  ldi  data, TurnR
+  sts UDR1, data ; Move data to transmit data buffer
+ret
+
 ;----------------------------------------------------------------
 ; Sub: Wait
 ; Desc: A wait loop that is 16 + 159975*waitcnt cycles or roughly
